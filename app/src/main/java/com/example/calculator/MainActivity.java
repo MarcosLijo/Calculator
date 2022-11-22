3f2d839df36a3b6ab8e3d8859d8209507d98d90e
Marcos <mlijo@nigul.cide.es>
2022-11-22 19:17:22 +0100

    Comentarios + arreglar

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Variables
     String operacion = "";
     String operador = "";
     int op1;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Botón igual deshabilitado para no poder escribir un igual sin operandos ni operadores
         Button bEq = findViewById(R.id.buttonEqual);
         bEq.setEnabled(false);
 
@@ -31,22 +33,32 @@ public class MainActivity extends AppCompatActivity {
         Button bEq = findViewById(R.id.buttonEqual);
         Button b0 = findViewById(R.id.button0);
 
-        if (resultado != 0) operacion = "";
+        // Si el resultado no es 0, la operación empieza de nuevo
+        if (resultado != 0) {
+            operacion = "";
+            resultado = 0;
+        }
+        // Cogemos el tag y se lo añadimos a la operación
         operacion += (String) view.getTag();
+        // Escribimos el texto en el TextView
         tv.setText(operacion);
 
+        // Si hay un operador, calculamos la operación existente en el segundo TextView
         if (!operador.equals("")) {
             calcular(tv2);
+            // Si no, cogemos el tag y se lo ponemos al segundo TextView también
         } else {
             text2 += (String) view.getTag();
             tv2.setText(text2);
         }
 
 
+        // Habilitamos algunos botones que hemos deshabilitado anteriormente
         bEq.setEnabled(true);
         b0.setEnabled(true);
     }
 
+    // Método para calcular la operación existente en el segundo TextView (Es igual a escribirResultado())
     private void calcular(TextView tv2) {
 
         String[] values = operacion.split("[-+*/%]+");
@@ -74,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         tv2.setText(text2);
+        text2 = "";
     }
 
 
@@ -83,10 +96,14 @@ public class MainActivity extends AppCompatActivity {
         Button bEq = findViewById(R.id.buttonEqual);
         // No se puede pulsar un operador si no hay un operando primero
         if (operacion.equals("")) return;
+
+        // No puede empezar con un número negativo
         if (operacion.startsWith("-")) return;
 
+        // Ponemos el resultado a 0
         resultado = 0;
 
+        // Si no hay operador se lo asignamos y lo escribimos en el TextView1
         if (operador.equals("")) {
             operador = (String) view.getTag();
             operacion += operador;
@@ -107,15 +124,16 @@ public class MainActivity extends AppCompatActivity {
         Button bEq = findViewById(R.id.buttonEqual);
 
         try {
+            // Hacemos un array con los valores de la operación que tenemos
             String[] values = operacion.split("[-+*/%]+");
 
+            // Se los asignamos a sus variables correspondientes
             op1 = Integer.parseInt(values[0]);
             op2 = Integer.parseInt(values[1]);
         } catch (Exception e) {
             return;
         }
-
-
+        // Switch para calcular el resultado
         switch (operador) {
             case "+":
                 resultado = op1 + op2;
@@ -133,25 +151,32 @@ public class MainActivity extends AppCompatActivity {
                 resultado = op1 % op2;
                 break;
             default:
-//                resultado = Integer.parseInt(aux);
                 break;
         }
+        // A la operación total le añadimos el resultado y lo escribimos en el TextView1
         operacion += "=" + resultado;
-
         tv.setText(operacion);
+        // Y la operación pasa a tener el valor del resultado obtenido
         operacion = String.valueOf(resultado);
 
 
+        // Volvemos a habilitar los botones desahabilitados anteriormente
         if (operador.equals("/") || operador.equals("%")) b0.setEnabled(true);
+
+        // Reiniciamos los operandos y el operador
         op1 = 0;
         op2 = 0;
         operador = "";
+
+        // Y deshabilitamos el botón igual
         bEq.setEnabled(false);
     }
 
     public void clearAll(View view) {
         TextView tv = findViewById(R.id.TextViewOperation);
         TextView tv2 = findViewById(R.id.TextViewResult);
+
+        // Se reinician todas las variables y los TextView correspondientes
         operacion = "";
         operador = "";
         op1 = 0;
